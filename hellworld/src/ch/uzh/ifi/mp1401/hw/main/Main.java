0db5cb059679416737effcb2ebeaa47672d3b2af
Christian Bosshard <cbossi1987@gmail.com>
2014-02-26 13:49:17 +0100

    Introducing Printer

--- hellworld/src/ch/uzh/ifi/mp1401/hw/main/Main.java
+++ hellworld/src/ch/uzh/ifi/mp1401/hw/main/Main.java
@@ -2,8 +2,8 @@ package ch.uzh.ifi.mp1401.hw.main;
 
 public class Main {
 
-  public static void main(final String[] args) {
-    System.out.println("Hello World!");
-  }
+	public static void main(final String[] args) {
+		Printer.print("Hello World!");
+	}
 
 }
---
+++ hellworld/src/ch/uzh/ifi/mp1401/hw/main/Printer.java
@@ -0,0 +1,9 @@
+package ch.uzh.ifi.mp1401.hw.main;
+
+public class Printer {
+	
+	public static void print(String text) {
+		System.out.println(text);
+	}
+
+}
